7a404407d3c1b7d4db2855ad1622243f5a0cb45d
KNOWLEDGELAKE\dominique.hallan <dominique.hallan@knowedgelake.com>
2018-01-05 08:11:07 -0600

    added code base from cl

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " " + "This number is divisable by both 3 and 5!");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " " + "This number is divisable by 3.");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " " + "This is number is divisable by 5.");
+                }
+                else
+                {
+                    Console.WriteLine(i + " " + "This number is not divisable by either 3 or 5, sad.");
+                   
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
